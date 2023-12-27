fbeae3d613ff04ff723c6dbf34315e5e01c9f862
yarqui <108986486+yarqui@users.noreply.github.com>
2023-12-27 16:23:07 +0000

    Deploying to gh-pages from @ yarqui/goit-js-hw-08@955853dded45023c713fbe6a8c43db0c0b788826 🚀

---
+++ assets/common-e457854e.js
@@ -0,0 +1,2 @@
+const o=r=>{try{return JSON.parse(localStorage.getItem(r))}catch(e){console.error(e.message)}},t=(r,e)=>localStorage.setItem(r,JSON.stringify(e)),a=r=>{localStorage.removeItem(r)};export{a,o as r,t as w};
+//# sourceMappingURL=common-e457854e.js.map
