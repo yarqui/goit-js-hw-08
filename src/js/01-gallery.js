14af7e6834605f3586af867fe0e64e75665c4c90
yarqui <y.pelykh@gmail.com>
2023-12-27 12:04:17 -0500

    edit simplelightbox import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,5 @@
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
