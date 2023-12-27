8fb36326cb6bb8141b4c1f9cb7cd97307708ff3c
yarqui <108986486+yarqui@users.noreply.github.com>
2023-12-27 18:01:30 +0000

    Deploying to gh-pages from @ yarqui/goit-js-hw-08@067b8bc339b865df2ba4b2877368644a3be19db1 🚀

--- assets/common-bfdb7e56.js
+++
@@ -1,2 +0,0 @@
-const t={currentTime:"videoplayer-current-time",feedbackForm:"feedback-form-state"},o=e=>{try{return JSON.parse(localStorage.getItem(e))}catch(r){console.error(r.message)}},a=(e,r)=>localStorage.setItem(e,JSON.stringify(r)),s=e=>{localStorage.removeItem(e)};export{t as L,s as a,o as r,a as w};
-//# sourceMappingURL=common-bfdb7e56.js.map
