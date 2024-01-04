116255bac5aa02b43f1f73b445ed97510e1720d1
yarqui <y.pelykh@gmail.com>
2024-01-04 18:26:52 -0500

    edit tests: - add checkForEmptyLocalStorage function - add the form submission tests

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -32,6 +32,12 @@ const checkStateOfLocalStorage = async (
   );
 };
 
+const checkForEmptyLocalStorage = async (page: Page, key: string) =>
+  await page.waitForFunction(e => {
+    console.log('LS: ', localStorage.getItem(e));
+    return localStorage.getItem(e) === null;
+  }, key);
+
 test.beforeEach(async ({ page, context }) => {
   // throttle the network requests if needed
   // await context.route('**', route => {
@@ -71,8 +77,8 @@ test.describe('Inputs', () => {
     const message = page.getByLabel('Message');
 
     // clear all inputs
-    await email.fill('');
-    await message.fill('');
+    await email.clear();
+    await message.clear();
 
     // check whether inputs are empty
     await expect(email).toBeEmpty();
@@ -113,5 +119,55 @@ test.describe('Inputs', () => {
     await expect(email).toHaveValue(INPUT_FILL.email);
     await expect(message).toHaveValue(INPUT_FILL.message);
     await checkStateOfLocalStorage(page, INPUT_FILL);
+  });
+
+  test.fixme(
+    'should clear inputs and local storage key on "submit button" click',
+    async ({ page }) => {
+      // FIXME: works fixme in --debug mode. Otherwise it doesn't clear the inputs
+      const email = page.getByLabel('Email');
+      const message = page.getByLabel('Message');
+      const submitBtn = page.getByRole('button', { name: 'Submit' });
+
+      await submitBtn.click();
+
+      await expect(email).toBeEmpty();
+      await expect(message).toBeEmpty();
+      await checkForEmptyLocalStorage(page, 'feedback-form-state');
+    }
+  );
+
+  test.fixme(
+    'should clear inputs and local storage key on "Enter", while "email" input is focused',
+    async ({ page }) => {
+      // FIXME: works only in --debug mode. Otherwise it doesn't clear the inputs
+      const email = page.getByLabel('Email');
+      const message = page.getByLabel('Message');
+
+      await email.press('Enter');
+
+      await expect(email).toBeEmpty();
+      await expect(message).toBeEmpty();
+      await checkForEmptyLocalStorage(page, 'feedback-form-state');
+    }
+  );
+
+  test("shouldn't submit if there are empty inputs ", async ({ page }) => {
+    const email = page.getByLabel('Email');
+    const message = page.getByLabel('Message');
+    const submitBtn = page.getByRole('button', { name: 'Submit' });
+
+    await message.clear();
+    await submitBtn.click();
+
+    await expect(email).not.toBeEmpty();
+    await expect(message).toBeEmpty();
+
+    await message.fill(INPUT_FILL.message);
+    await email.clear();
+    await email.press('Enter');
+
+    await expect(email).toBeEmpty();
+    await expect(message).not.toBeEmpty();
   });
 });
